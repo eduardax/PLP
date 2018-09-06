e8db09a59bd0d0ecaa59156ea8df7eab31f8a99c
Mateus Henrique <mateushenriquechaves1@gmail.com>
2018-09-05 22:58:32 -0300

    Criticando o teu código
    KDJAKSJD

--- Part2.py
+++ Part2.py
@@ -13,27 +13,29 @@ def create(nome, cpf, curso, rg, endereco, hora_entrada, hora_saida, celular, ma
         'matricula' : matricula,
     })
 
-print "\t\t\tTreinee TEC\n"
+print ("\t\t\tTreinee TEC\n")
 
-print "1-Create\n2-Read\n3-Upload\n4-Delete\n5-Exit\n"
-op = input("Digite sua opcao:\n")
-
-if op==1:
-    print "\t\tCADASTRO\n"
-    name = input("Nome:\n")
-    cpf = input("CPF:\n")
-    curso = input("Curso:\n")
-    rg = input("Rg:\n")
-    endereco = input("Endereco:\n")
-    hora_entrada = input("Hora de Entrada:\n")
-    hora_saida = input("Hora de Saída:\n")
-    celular = input("Celular:\n")
-    matricula = input("Matricula:\n")
-    create (nome, cpf, curso, rg, endereco, hora_entrada, hora_saida, celular, matricula):
-if op==2:
-    print "\n\t\tEstagiarios Cadastrados\n"
-    for i in estagiarios:
-        for i in i.items():
-            print ('Nome: {} : Matricula: {}'. format (nome), (matricula))
-        print('\n')
-    op2 = input("")
+while True:
+    print ("1-Create\n2-Read\n3-Upload\n4-Delete\n5-Exit\n")
+    op = int(input("Digite sua opcao:\n"))
+    if(op == 5):
+        break
+    if op == 1:
+        print ("\t\tCADASTRO\n")
+        name = input("Nome:\n")
+        cpf = input("CPF:\n")
+        curso = input("Curso:\n")
+        rg = input("Rg:\n")
+        endereco = input("Endereco:\n")
+        hora_entrada = input("Hora de Entrada:\n")
+        hora_saida = input("Hora de Saída:\n")
+        celular = input("Celular:\n")
+        matricula = input("Matricula:\n")
+        create(name, cpf, curso, rg, endereco, hora_entrada, hora_saida, celular, matricula)
+    if op==2:
+        print("\n\t\tEstagiarios Cadastrados\n")
+        for i in estagiarios:
+            for i in i.items():
+                print('{} : {}'. format(i[0], i[1]))
+            print('\n')
+        op2 = input("")
